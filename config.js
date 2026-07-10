bf25b83dea170c12abee50ed31ce512054b41b13
Ricardo Gulias <gulias@DAREDE-001430.darede.corp>
2026-07-10 14:47:07 -0300

    feat: add Members Lounge + Admin Panel + config.js for USA chapter

---
+++ config.js
@@ -0,0 +1,5 @@
+var GJ_CONFIG = {
+    ADMIN_EMAILS: ['aws42@icloud.com', 'ricardo.gulias@goldenjacketsbrazil.com'],
+    ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin',
+    CHAPTER: 'usa'
+};
